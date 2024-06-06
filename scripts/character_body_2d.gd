e450743cc7601a1ac95183a0ca1001ae11c6f603
Manu <manulopez542@gmail.com>
2024-06-05 22:11:16 -0300

    animations made with animationTree

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,45 +1,65 @@
 extends CharacterBody2D
 
-@onready var animationPlayer = $AnimatedSprite2D
+@onready var animatedSprite = $AnimatedSprite2D
+@onready var AnimTree = $AnimationTree
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
+var direction : float = 0.0
 
 var is_Attacking = false
 
 func _is_attacking():
-	return animationPlayer.animation == "attack" and animationPlayer.is_playing()
+	return animatedSprite.animation == "attack" and animatedSprite.is_playing()
+
+func _process(_delta: float) -> void:
+	_handle_animations()
 
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		if velocity.y > 0:
-			animationPlayer.play("fall")
-		else:
-			animationPlayer.play("jump")
 
 	if not _is_attacking():
 		# Handle jump.
-		if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		if Input.is_action_just_pressed("ui_up") and is_on_floor():
 			velocity.y = JUMP_VELOCITY
 			
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		
-		var direction = Input.get_axis("ui_left", "ui_right")
-		animationPlayer.flip_h = direction < 0 if direction else animationPlayer.flip_h
+		direction = Input.get_axis("ui_left", "ui_right")
 		
 		if Input.is_action_just_pressed("key_Z") and is_on_floor():
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			animationPlayer.play("attack")
 		elif direction:
 			velocity.x = direction * SPEED
-			if is_on_floor():
-				animationPlayer.play("run")
 		else:
 			velocity.x = move_toward(velocity.x, 0, SPEED)
-			if is_on_floor():
-				animationPlayer.play("idle")
 
 	move_and_slide()
+
+func _handle_animations():
+	animatedSprite.flip_h = direction < 0 if direction else animatedSprite.flip_h
+	if is_on_floor():
+		AnimTree["parameters/conditions/is_on_floor"] = true
+		AnimTree["parameters/conditions/is_jump"] = false
+		AnimTree["parameters/conditions/is_fall"] = false
+		if Input.is_action_just_pressed("key_Z"):
+			AnimTree["parameters/conditions/is_attack"] = true
+		elif direction:
+			AnimTree["parameters/conditions/is_attack"] = false
+			AnimTree["parameters/conditions/is_moving"] = true
+			AnimTree["parameters/conditions/idle"] = false
+		elif not direction:
+			AnimTree["parameters/conditions/is_attack"] = false
+			AnimTree["parameters/conditions/idle"] = true
+			AnimTree["parameters/conditions/is_moving"] = false
+	else :
+		AnimTree["parameters/conditions/is_on_floor"] = false
+		if velocity.y < 0:
+			AnimTree["parameters/conditions/is_jump"] = true
+			AnimTree["parameters/conditions/is_fall"] = false
+		elif velocity.y > 0:
+			AnimTree["parameters/conditions/is_jump"] = false
+			AnimTree["parameters/conditions/is_fall"] = true
